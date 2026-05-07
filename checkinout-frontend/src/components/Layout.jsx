cabf9cc411ca44829bb7f297a1100554c622bdb9
manuelmontoyar <manuel.montoya.r13@gmail.com>
2026-05-07 06:52:39 -0500

    feat: vistas SST Personal, fixes sidebar sticky y rutas perfil/notificaciones SST y Encargados

--- checkinout-frontend/src/components/Layout.jsx
+++ checkinout-frontend/src/components/Layout.jsx
@@ -4,8 +4,10 @@ import Sidebar from "./Sidebar";
 export default function Layout() {
   return (
     <div className="flex min-h-screen" style={{ background: "var(--co-bg)" }}>
-      <Sidebar />
-      <main className="flex-1 flex flex-col min-w-0">
+      <div className="sticky top-0 h-screen flex-shrink-0">
+        <Sidebar />
+      </div>
+      <main className="flex-1 flex flex-col min-w-0 overflow-y-auto">
         <Outlet />
       </main>
     </div>
