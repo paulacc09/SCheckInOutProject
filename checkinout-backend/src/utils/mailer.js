69fe8807ea32d52363f169d8284172305233756b
paulacc09 <paulacostacc11@gmail.com>
2026-05-11 01:28:30 -0500

    fix: mailer reestablecido a gmail

--- checkinout-backend/src/utils/mailer.js
+++ checkinout-backend/src/utils/mailer.js
@@ -1,8 +1,6 @@
 const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: { user: process.env.MAIL_USER, pass: process.env.MAIL_PASS },
 });
 module.exports = transporter;
