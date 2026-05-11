4838ea931a0c02070a9fca036884a5315ba568e3
paulacc09 <paulacostacc11@gmail.com>
2026-05-11 01:21:52 -0500

    fix: mailer usa puerto 587 para compatibilidad con Railway

--- checkinout-backend/src/utils/mailer.js
+++ checkinout-backend/src/utils/mailer.js
@@ -1,11 +1,8 @@
 const nodemailer = require("nodemailer");
-
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
-  },
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: { user: process.env.MAIL_USER, pass: process.env.MAIL_PASS },
 });
-
 module.exports = transporter;
